af5ea0637079bfc2c22d454f96993ed7130574bc
SHIRO <39456646+shiro46mt@users.noreply.github.com>
2025-07-13 22:11:17 +0900

    init

---
+++ src/mhlw.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+import re
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+
+# requests用パラメータ
+headers = {'User-Agent': ''}
+timeout_sec = 60
+
+
+def get_file_urls():
+    # 医療保険が適用される医薬品について
+    top_url = 'https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000078916.html'
+    html = requests.get(top_url, timeout=timeout_sec, headers=headers)
+    html.raise_for_status()
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    # 薬価基準収載品目リストについて へのリンク
+    page_url_tags = soup.find_all('a', string=re.compile('薬価基準収載品目リストについて'))
+    page_urls = [urljoin(top_url, tag.attrs['href']) for tag in page_url_tags]
+
+    # 薬価基準収載品目リストについて の各ページ
+    file_urls = {}
+    for page_url in page_urls:
+        # 年度の取得
+        mob = re.search(r'/tp(\d{4})\d{4}-01.html', page_url)
+        if mob:
+            year = mob.group(1)
+        else:
+            continue
+
+        # ページの取得
+        html = requests.get(page_url, timeout=timeout_sec, headers=headers)
+        html.raise_for_status()
+        soup = BeautifulSoup(html.content, 'html.parser')
+
+        # ダウンロード用リンクの取得
+        file_url_tags = soup.select('#contents .ico-excel a')[:5]
+        file_urls[year] = [urljoin(page_url, tag.attrs['href']) for tag in file_url_tags]
+
+    return file_urls
+
+
+def download_price(year, file_urls: list[str]):
+    """厚労省HPから、(1)-(4)薬価基準収載品目リストの一覧ファイルをダウンロードし、csv形式 (UTF-8) で保存する。
+
+    Args:
+        file_urls:
+    """
+    pattern = re.compile(r"(tp(\d{8})-01_0?[1234].xlsx?)")
+    dfs = []
+    max_update = '00000000'
+
+    for file_url in file_urls:
+        # ファイル名の確認
+        mob = pattern.search(file_url)
+        if mob is None:
+            continue
+
+        # ファイルの読み込み
+        df = pd.read_excel(file_url, dtype=str)
+        # df['file'] = mob.group(1)
+        dfs.append(df)
+
+        # 更新日の更新
+        max_update = max(max_update, mob.group(2))
+
+    # データフレームの連結
+    df = pd.concat(dfs)
+
+    # 列名の矯正
+    df = df.rename(columns={'Unnamed: 4': '日本薬局方', 'Unnamed: 5': '麻薬', 'Unnamed: 6': '業者名追記'})
+
+    # バリデーション
+    assert set(df['区分'].unique()) == set(['内用薬', '注射薬', '外用薬', '歯科用薬剤'])
+
+    # csvの出力
+    filepath = Path(f'data/mhlw_price/{year}/{max_update}.csv')
+    if not filepath.parent.is_dir():
+        filepath.parent.mkdir()
+    df.to_csv(filepath, index=False, encoding='utf8')
+
+
+def download_ge(year, file_urls: list[str]):
+    """厚労省HPから、(5)後発医薬品に関する情報の一覧ファイルをダウンロードし、csv形式 (UTF-8) で保存する。
+
+    Args:
+        file_urls:
+    """
+    pattern = re.compile(r"(tp(\d{8})-01_0?5.xlsx?)")
+
+    file_url = max([f for f in file_urls if pattern.search(f)])
+
+    # ファイル名の確認
+    mob = pattern.search(file_url)
+
+    # ファイルの読み込み
+    df = pd.read_excel(file_url, dtype=str)
+    # df['file'] = mob.group(1)
+
+    # 列名の矯正
+    df = df.rename(columns={'収載年月日(YYYYMMDD)\n【例】\n2016年4月1日\n(20160401)': '収載年月日'})
+
+    # バリデーション
+
+    # csvの出力
+    filepath = Path(f'data/mhlw_ge/{year}/{mob.group(2)}.csv')
+    if not filepath.parent.is_dir():
+        filepath.parent.mkdir()
+    df.to_csv(filepath, index=False, encoding='utf8')
+
+
+def main():
+    file_urls = get_file_urls()
+    for year, urls in file_urls.items():
+        download_price(year, urls)
+        download_ge(year, urls)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/y.py
@@ -0,0 +1,103 @@
+from io import BytesIO
+from pathlib import Path
+import re
+from urllib.parse import urljoin
+import zipfile
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+
+# requests用パラメータ
+headers = {'User-Agent': ''}
+timeout_sec = 60
+
+
+def get_file_urls():
+    # トップページ
+    top_url = 'https://shinryohoshu.mhlw.go.jp/shinryohoshu/downloadMenu/'
+    html = requests.get(top_url, timeout=timeout_sec, headers=headers)
+    html.raise_for_status()
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    # 各年度の診療報酬改定 へのリンク
+    page_url_tags = soup.find_all('a', string=re.compile('こちら'))
+    page_urls = [urljoin(top_url, tag.attrs['href']) for tag in page_url_tags]
+    page_url = max([url.split(';')[0] for url in page_urls if '/kaitei/' in url])
+
+    # 昨年度の診療報酬海底 のページ
+    file_urls = {}
+
+    # 年度の取得
+    g = page_url[-3]
+    if g == 'R':
+        year = int(page_url[-2:]) + 2018
+    else:
+        raise ValueError
+
+    # ページの取得
+    html = requests.get(page_url, timeout=timeout_sec, headers=headers)
+    html.raise_for_status()
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    # ダウンロード用リンクの取得
+    file_url_tag = soup.find('a', string=re.compile('医薬品マスター'))
+    file_urls[str(year)] = urljoin(page_url, file_url_tag.attrs['href'])
+
+    # 最新年度のダウンロード用リンク
+    file_urls[str(year+1)] = 'https://shinryohoshu.mhlw.go.jp/shinryohoshu/downloadMenu/yFile'
+
+    return file_urls
+
+
+def download_y(year, file_url: str):
+    """診療報酬情報提供サービスから、医薬品マスターの一覧ファイルをダウンロードし、csv形式 (UTF-8) で保存する。"""
+    pattern = re.compile(r"y_(\d{8}).csv")
+
+    # ファイルダウンロード
+    r = requests.get(file_url, stream=True)
+    r.raise_for_status()
+    zip_content = BytesIO(r.content)
+
+    # zipファイルの中のcsvファイルを読み込み
+    with zipfile.ZipFile(zip_content, 'r') as zf:
+        for filename in zf.namelist():
+            # ファイル名の確認
+            mob = pattern.search(filename)
+            if mob is None:
+                continue
+
+            # 更新日の更新
+            update = mob.group(1)
+
+            # ファイルの読み込み
+            with zf.open(filename) as cf:
+                csv_bytes = BytesIO(cf.read())
+                df = pd.read_csv(csv_bytes, dtype=str, header=None, encoding='cp932')
+                break
+
+    # バリデーション
+
+
+    # csvの出力
+    filepath = Path(f'data/y/{year}/{update}.csv')
+    if not filepath.parent.is_dir():
+        filepath.parent.mkdir()
+
+    ## ヘッダ行
+    filepath_header = Path(__file__).parent / 'y_header.csv'
+    filepath.write_bytes(filepath_header.read_bytes())
+
+    ## データ
+    df.to_csv(filepath, index=False, header=False, encoding='utf8', mode='a')
+
+
+def main():
+    file_urls = get_file_urls()
+    for year, url in file_urls.items():
+        download_y(year, url)
+
+
+if __name__ == '__main__':
+    main()
