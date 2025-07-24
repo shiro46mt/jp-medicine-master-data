2c5bce0e83f887b0322aea57f6a217a7bf28db81
SHIRO <39456646+shiro46mt@users.noreply.github.com>
2025-07-24 22:28:38 +0900

    データカタログの更新前に差分確認を追加

--- src/data_catalog.py
+++ src/data_catalog.py
@@ -6,20 +6,27 @@ from pathlib import Path
 # 出力先
 data_dir = Path(__file__).parents[1] / 'data'
 
-cataog = {
+catalog = {
     'update': datetime.now().strftime('%Y-%m-%d'),
     'data': {},
 }
 
+# csvファイルの一覧を取得
 data_subdirs = sorted(data_dir.glob('*'))
 for d in data_subdirs:
     if d.is_dir():
-        cataog['data'][d.name] = []
+        catalog['data'][d.name] = []
 
         files = sorted(d.glob('**/*.csv'))
         for f in files:
-            cataog['data'][d.name].append(f.relative_to(d).as_posix())
+            catalog['data'][d.name].append(f.relative_to(d).as_posix())
 
+# 変更の有無の確認
 filepath = data_dir / 'data_catalog.json'
-with open(filepath, 'w', newline='', encoding='utf8') as f:
-    json.dump(cataog, f, indent=4, ensure_ascii=False)
+with open(filepath, 'r', encoding='utf8') as f:
+    catalog_prev = json.load(f)
+
+# 変更があれば出力
+if catalog['data'] != catalog_prev['data']:
+    with open(filepath, 'w', newline='', encoding='utf8') as f:
+        json.dump(catalog, f, indent=4, ensure_ascii=False)
