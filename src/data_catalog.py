e72bc2f26c68dd90f5727e8ac2d3696872692a64
SHIRO <39456646+shiro46mt@users.noreply.github.com>
2025-10-18 15:45:11 +0900

    データカタログの仕様変更

--- src/data_catalog.py
+++ src/data_catalog.py
@@ -6,20 +6,31 @@ from pathlib import Path
 # 出力先
 data_dir = Path(__file__).parents[1] / 'data'
 
+# カタログ情報
+catalog_info_fielpath = Path(__file__).parent / 'data_catalog_info.json'
+catalog_info = json.loads(catalog_info_fielpath.read_text(encoding='utf8'))
+
+# カタログの初期化
 catalog = {
     'update': datetime.now().strftime('%Y-%m-%d'),
-    'data': {},
+    'data': [],
 }
 
 # csvファイルの一覧を取得
 data_subdirs = sorted(data_dir.glob('*'))
 for d in data_subdirs:
     if d.is_dir():
-        catalog['data'][d.name] = []
+        # カタログアイテムの初期化
+        item = catalog_info.get(d.name, {})
+        item['files'] = []
 
+        # ファイルの一覧のリスト化
         files = sorted(d.glob('**/*.csv'))
         for f in files:
-            catalog['data'][d.name].append(f.relative_to(d).as_posix())
+            item['files'].append(f.relative_to(d).as_posix())
+
+        # カタログアイテムの追加
+        catalog['data'].append(item)
 
 # 変更の有無の確認
 filepath = data_dir / 'data_catalog.json'
